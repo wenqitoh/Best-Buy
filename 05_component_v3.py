1c8e861ee469e1c35ffdf856bc1570222948c7b4
wenqitoh <tohw@middleton.school.nz>
2021-09-05 21:47:25 +1200

    Component 5

--- 05_component_v3.py
+++ 05_component_v3.py
@@ -1,7 +1,9 @@
-"""Component 5 v2 - allow for multiple min/max values,
+"""Component 5 v3 - allow for multiple min/max values,
 output the multiple values not just pick one
+use a set, so that both name and price is printed not just price - more user
+friendly
 Created by Wen-Qi Toh
-16/8/21"""
+5/9/21"""
 
 # for testing purposes, will be using sample data, not user input for list
 product_list = [[500, 'Essentials Cheese Block Cheddar', 6.4, 0.01],
@@ -11,25 +13,27 @@ product_list = [[500, 'Essentials Cheese Block Cheddar', 6.4, 0.01],
                 [130, 'Meyer Semi Soft Cheese Cumin Gouda', 7.4, 0.06],
                 [500, 'Countdown Cheese Block Colby', 8.2, 0.02]]
 
+
 cheapest = []
 costliest = []
 
 for product in product_list:
     # this line account for an empty list OR a lower price
-    if not cheapest or product[2] < cheapest[0][2]:
-        cheapest = [product]
-
-    elif product[2] == cheapest[0]:
-        cheapest.append(product)
-
+    if not cheapest or product[2] < cheapest[0][1]:
+        cheapest = [(product[1], product[2])]
+    elif product[2] == cheapest[0][1]:
+        cheapest.append((product[1], product[2]))
     # this line account for an empty list OR a higher price
-    if not costliest or product[2] > costliest[0][2]:
-        costliest = [product]
+    if not costliest or product[2] > costliest[0][1]:
+        costliest = [(product[1], product[2])]
 
-    elif product[2] == costliest[0]:
-        costliest.append(product)
+    elif product[2] == costliest[0][1]:
+        costliest.append((product[1], product[2]))
 
 # display min/max
-print(f"cheapest: {cheapest}")
-print(f"costliest: {costliest}")
-
+print("cheapest product:")
+for s in cheapest:
+    print(", ".join(str(e) for e in s))
+print("\ncostliest product:")
+for s in costliest:
+    print(", ".join(str(e) for e in s))
