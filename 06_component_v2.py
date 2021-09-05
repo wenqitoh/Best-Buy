eaaec00689114ab4875ddc0327b35623978ab554
wenqitoh <tohw@middleton.school.nz>
2021-09-05 16:30:57 +1200

    THIS ONE COUSIN!

--- 06_component_v2.py
+++ 06_component_v2.py
@@ -8,37 +8,71 @@ Created by Wen-Qi Toh
 # ...program, after user has entered 'X'
 
 budget = 5.00  # set data for testing purposes only
-unit_price_list = [0.05, 0.05, 0.05, 0.02, 0.01, 0.02]
+unit_price_list = [0.01, 0.05, 0.05, 0.01, 0.02, 0.01, 0.02]
 # no need to check these lists are correct, as any errors would have alr...
 # ...been filtered out by the earlier parts of the program
-product_list = [[250, 'ch1', 12, 0.05],
+product_list = [[250, 'ch1', 12, 0.01],
                 [350, 'ch2.5', 12, 0.05],
                 [110, 'ch3', 5, 0.05],
+                [112, 'ch3.5', 4.5, 0.01],
                 [200, 'ch4', 3.99, 0.02],
+                [230, 'ch5', 2.45, 0.01],
                 [230, 'ch5', 2.45, 0.01],
                 [250, 'ch6', 6, 0.02]]    # set data for testing purposes only
 
 best_buy = []
+new_product_list = []
+new_upl = []
 
-for product in product_list:    # so basically my issue is that when u run this prg,
-            # it doesn't print out if ch2 is accepted or not? I've tried with different numbers and everything like that,
-            # but this prg always skips the 2nd value in the product list. also it does it sometimes w/ the last value, idk why
+for product in product_list:
     if product[2] > 5:  # step 1
-        product_list.remove(product)
-        unit_price_list.remove(product[3])
-
+        # product_list.remove(product)
         print("The product '{}' has been removed from product list as "
               "the price exceeds the budget.\n".format(product[1]))
 
     elif product[2] <= 5:   # step 2
-        print("product {} accepted\n".format(product[1]))
-        if product[3] < min(unit_price_list):
-            best_buy = product
-            print("if {}".format(best_buy))
-        elif product[3] == min(unit_price_list):
-            best_buy.append(product)
-            print("product {} accepted".format(product[1]))
-            print("best buy so far: {}\n".format(best_buy))
+        print("\nproduct {} accepted".format(product[1]))
+        new_product_list.append(product)
+        new_upl.append(product[3])
+        print("new product list:{}".format(new_product_list))
+        print("new unit price list: {}".format(new_upl))
 
+for nproduct in new_product_list:
+    # this line account for an empty list OR a lower price
+    if not best_buy or nproduct[3] < best_buy[0][3]:
+        best_buy = [nproduct]
+    elif nproduct[3] == best_buy[0][3]:
+        best_buy.append(nproduct)
+
+count = 0
 # final output
-print("The recommended best buy/s for you is: {}".format(best_buy))
+print("The recommended best buy/s for you is:\n{}\n".format(best_buy))
+
+# print(*best_buy, sep = "\n")
+for item in best_buy:
+    print(' '.join(str(x) for x in item))
+
+for number, letter in enumerate(best_buy):
+    print(number, ' '.split(letter))
+
+        # if product[3] < new_upl[0]:
+        #     best_buy.clear()
+        #     best_buy.append(product)
+        #     new_upl.remove(new_upl[0])
+        #     print("best buy for if stmnt: {}".format(best_buy))
+        #     print("new upl removed: {}".format(new_upl))
+        #     # best_buy = product
+        #
+        # elif product[3] == min(new_upl):
+        #     index = new_upl.index(min(new_upl))
+        #     print(index)
+        #     new_upl.remove(index)
+        #     new_upl.append(product[3])
+        #     new_upl.append(product[3])
+        #     best_buy.append(product)
+        #     print("best buy so far: {}".format(best_buy))
+
+
+
+
+
